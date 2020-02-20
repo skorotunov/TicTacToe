8c012e793fe2facb1086c3bde5c7ddd18c5efb56
Sergiy Korotunov <skorotunov@outlook.com>
2020-02-20 13:48:04 +0200

    Implement authorization both at Angular and .NET Core sides

--- WebUI/ClientApp/src/app/app.module.ts
+++ WebUI/ClientApp/src/app/app.module.ts
@@ -6,9 +6,7 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { PlayersComponent } from './players/players.component';
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
@@ -17,9 +15,7 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    PlayersComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -27,9 +23,8 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     FormsModule,
     ApiAuthorizationModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
+      { path: '', redirectTo: '/players', pathMatch: 'full' },
+      { path: 'players', component: PlayersComponent, canActivate: [AuthorizeGuard] },
     ])
   ],
   providers: [
---
+++ WebUI/ClientApp/src/app/players/players.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-players',
+  templateUrl: './players.component.html',
+})
+export class PlayersComponent {
+}
